ab86168e703103055ba116ce7f6b66322db28404
Sanji07Vinsmoke <adriangwapo140@gmail.com>
2025-03-02 16:41:11 +0800

    Delete api directory

--- api/contracts/StudentRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface StudentRepositoryInterface {
-    public function AddStudent($StudentName, $MidtermScore, $FinalScore);
-    public function GetAllStudents();
-    public function GetStudentById($StudentId);
-    public function UpdateStudent($StudentId, $MidtermScore, $FinalScore);
-    public function DeleteStudent($StudentId);
-}
--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'core/Router.php';
-
-// Capture request URI
-$request_uri = trim($_GET['route'] ?? '', '/');
-
-$router = new Router();
-$router->HandleRequest($request_uri);
-?>
